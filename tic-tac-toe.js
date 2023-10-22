7a287da36c2cf8c1187ec014d9e975b2859ead5c
kkbrown123 <brownkevonteh@gmail.com>
2023-10-22 10:14:00 -0500

    added x or o when square is clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,11 +1,42 @@
 document.addEventListener('DOMContentLoaded', function() {
     let board = document.getElementById("board");
     let board_div = board.querySelectorAll("div");
+    const squares = Array.from(board.getElementsByTagName("div"))
+    let moves =
+    [" "," "," ", //row1
+    " "," "," " ,  //row2
+    " "," "," "] //row3
+    let X_play = document.createTextNode("X"); 
+    let O_play = document.createTextNode("O");
+    let cur_play = "X";
 
 
 
     board_div.forEach((sq) =>{
         sq.classList.add("square");
+        sq.addEventListener("click",function(){
+        
+            if(moves[squares.indexOf(sq)] == " " ){
+              if(cur_play == "X"){
+    
+               sq.append(X_play);   
+               sq.classList.add('square', 'X');
+               moves[squares.indexOf(sq)] = cur_play
+               sq.innerText = cur_play;
+              
+               cur_play = "O";
+             }else{
+              
+               sq.append(O_play);
+               sq.classList.add('square', 'O');
+               moves[squares.indexOf(sq)] =cur_play
+               sq.innerText = cur_play;
+               cur_play = "X";
+              
+             }
+            }
+          
+          });
         
           
     
