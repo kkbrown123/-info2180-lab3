876c50a3b5bc84f143e218ad470a4780258f430f
kkbrown123 <brownkevonteh@gmail.com>
2023-10-22 16:34:50 -0500

    game restart feature added

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,49 +3,50 @@ document.addEventListener('DOMContentLoaded', function() {
     let board_div = board.querySelectorAll("div");
     let status = document.getElementById("status");
     const squares = Array.from(board.getElementsByTagName("div"));
-    let newgame_button = document.getElementsByClassName("btn")[0];
+    
+    let gameOn = true;
+    let newGameBtn = document.getElementsByClassName("btn")[0];
+
+    //Set array to store record of game 
     let moves =
     [" "," "," ", //row1
     " "," "," " ,  //row2
     " "," "," "] //row3
 
+    //Set starting player
     let cur_play = "X";
-
-
 
     board_div.forEach((sq) =>{
         sq.classList.add("square");
         sq.addEventListener("click",function(){
-            // console.log(moves[squares.indexOf(sq)]);
-            if(moves[squares.indexOf(sq)] == " " ){
-               
+           
+            if(moves[squares.indexOf(sq)] == " " && gameOn == true ){
               if(cur_play == "X"){
                sq.classList.add('square', 'X');
-               moves[squares.indexOf(sq)] = cur_play
-               sq.innerText = cur_play;
+               moves[squares.indexOf(sq)] = cur_play;
+               sq.textContent = cur_play;
                if(checkWin(squares.indexOf(sq))== true){
-                console.log("X won");
-                status.innerText = "Congratulations! " + cur_play + " is the Winner!"
-                status.classList.add("you-won")
+                status.textContent = "Congratulations! " + cur_play + " is the Winner!"
+                status.classList.add("you-won");
+                gameOn = false;
                }else{
                 cur_play = "O";
+                console.log("Now it is " + cur_play +" turn");
                }
               
                
                
-             }else if (cur_play == "O"){
-              
-              
+             }else if (cur_play == "O"  && gameOn == true){
                sq.classList.add('square', 'O');
                moves[squares.indexOf(sq)] =cur_play
-               sq.innerText = cur_play;
-              
+               sq.textContent = cur_play;
                if(checkWin(squares.indexOf(sq))== true){
-                console.log("O won");
-                status.innerText = "Congratulations! " + cur_play + " is the Winner!"
-                status.classList.add("you-won")
+                status.textContent = "Congratulations! " + cur_play + " is the Winner!"
+                status.classList.add("you-won");
+                gameOn = false;
                }else{
                 cur_play = "X";
+                console.log("Now it is " + cur_play +" turn");
                }
               
              }
@@ -63,9 +64,11 @@ document.addEventListener('DOMContentLoaded', function() {
           
     
       });
+
+//Checks each the squares to see if player has won
  let checkWin = function(square){
     let playerMove = moves[square];
-    console.log(playerMove + " " + square);
+    
     if(square == 0){
         if ((moves[1] == playerMove & moves[2] == playerMove) || (moves[3] == playerMove & moves[6] == playerMove) || (moves[4] == playerMove && moves[8] == playerMove))
             {return true}
@@ -108,21 +111,35 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
  }
- newgame_button.addEventListener("click",function(){
-   
-    squares.forEach((sq) =>{
-       sq.innerText = " "
-        
-        
+
+ newGameBtn.addEventListener("click", () => {
+    console.log("CLICKED NEW GAME")
+    squares.forEach(square => {
+        if(square.textContent == "X"){
+            square.classList.remove('X');
+            square.textContent = " ";
+
+        }else{
+            square.classList.remove('O');
+            square.textContent = " ";
+        }
+       
     });
     moves =
     [" "," "," ", //row1
     " "," "," " ,  //row2
-    " "," "," "]
-    status.innerText = "Move your mouse over a square and click to play an X or an O.";
+    " "," "," "];
+
+    status.textContent = "Move your mouse over a square and click to play an X or an O.";
     status.classList.remove("you-won");
+    
     cur_play = "X";
- });
+    gameOn = true;
+    
+    
+
+})
+ 
 
 
 
