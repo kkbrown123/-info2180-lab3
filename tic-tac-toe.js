0fb75b9f4b2ca060bc5f14a3a71fe7cc5c0e1def
kkbrown123 <brownkevonteh@gmail.com>
2023-10-22 10:09:33 -0500

    add squares to page

---
+++ tic-tac-toe.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let board = document.getElementById("board");
+    let board_div = board.querySelectorAll("div");
+
+
+
+    board_div.forEach((sq) =>{
+        sq.classList.add("square");
+        
+          
+    
+      });
+
+
+
+});
